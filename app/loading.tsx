76cf2f33bb6eb54315d99093ff69295cfaebff13
Masaya <masaya.nishimura0927@gmail.com>
2025-12-06 11:34:14 +0900

    fix loading page design

--- app/loading.tsx
+++ app/loading.tsx
@@ -8,12 +8,12 @@ import { Spinner } from "@/components/ui/spinner";
 export default function Loading() {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <Card className="w-sm mx-6 md:w-md">
+      <Card className="w-sm mx-10 md:w-md">
         <div className="mb-2 flex items-center justify-center gap-2 text-primary">
-          <IconNotebook className="size-10 bg-red" />
-          <span className="font-semibold text-2xl">Habit Log</span>
+          <IconNotebook className="size-6 bg-red" />
+          <span className="font-semibold text-xl">Habit Log</span>
         </div>
-        <CardContent className="p-5 flex flex-col w-full text-primary items-center justify-center gap-4">
+        <CardContent className="py-10 flex flex-col w-full text-primary items-center justify-center gap-4">
           <Spinner className="size-20"/>
           <span className="text-xl">Loading...</span>
         </CardContent>
